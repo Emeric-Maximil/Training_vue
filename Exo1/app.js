dac21bb08a39dc9ba574a1ec11504b034a749a79
Emeric-Maximil <maximil.emeric@gmail.com>
2023-11-28 14:26:21 +0100

    Premier exercice mini tuto vue

---
+++ Exo1/app.js
@@ -0,0 +1,11 @@
+let leInput = document.getElementById('todo');
+let leButton = document.querySelector('button');
+let laList = document.querySelector('ul');
+
+leButton.addEventListener("click", ajouterTask)
+
+function ajouterTask(){
+    let newLi = document.createElement('li');
+    newLi.textContent = leInput.value;
+    laList.appendChild(newLi);
+}
