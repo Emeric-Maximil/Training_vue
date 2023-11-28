f9eda77b137309aae999fc5cbcccb895ad029d6e
Emeric-Maximil <maximil.emeric@gmail.com>
2023-11-28 16:20:20 +0100

    Exo 2 Tuto & Exo 3

--- Exo1/app.js
+++ Exo1/app.js
@@ -2,7 +2,7 @@ let leInput = document.getElementById('todo');
 let leButton = document.querySelector('button');
 let laList = document.querySelector('ul');
 
-leButton.addEventListener("click", ajouterTask)
+leButton.addEventListener("click", ajouterTask);
 
 function ajouterTask(){
     let newLi = document.createElement('li');
