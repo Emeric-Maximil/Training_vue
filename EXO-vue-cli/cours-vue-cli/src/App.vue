d5df3fe54f854dd9ec803786e78d9340daff6c4a
Emeric-Maximil <maximil.emeric@gmail.com>
2023-12-13 11:38:00 +0100

    repared ???

---
+++ EXO-vue-cli/cours-vue-cli/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- <div class="container"> -->
+    <h1 class="text-center">Les Amis</h1>
+    <ul class="list-group">
+      
+      <un-ami v-for="unAmi in lesAmis" 
+      :key=unAmi.id  
+      :leNom=unAmi.name 
+      :lePhone=unAmi.phone 
+      :leMail=unAmi.email 
+      :premium=unAmi.premium></un-ami>
+      <!--
+      <un-ami premium=false leNom="Jerome" lePhone="0635587894" leMail="martin.matin@"></un-ami>   
+      <un-ami leNom="Qui?" lePhone="0635587894" leMail="martin.matin@"></un-ami>   
+        -->
+    </ul>
+  <!-- </div> -->
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      lesAmis: [
+        {
+            id: 'lasticot',
+            name: 'COCO L ASTICOT',
+            phone: '01234 5678 991',
+            email: 'coco@lasticot.com',
+        },
+        {
+            id: 'janine',
+            name: 'Janine DeLavega',
+            phone: '09876 543 221',
+            email: 'janine@delavega.com',
+        },
+        {
+            id: 'janine',
+            name: 'Camille Elle',
+            phone: '09876 543 221',
+            email: 'janine@delavega.com',
+        },
+        {
+            id: 'janine',
+            name: 'BOnsoir',
+            phone: '09876 543 221',
+            email: 'janine@delavega.com',
+        },
+    ],
+    }
+  },
+}
+</script>
---
+++ EXO-vue-cli/cours-vue-cli/src/components/UnAmi.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container my-1">
+        <ul class="list-group">
+            <h2 class="list-group-item">{{leNom}}</h2> 
+            <h2 class="list-group-item">{{premiumData?"Ami Premium":"Ami Nul"}}</h2> 
+            <button @click="afficherDetails" class="btn btn-primary">Voir Détails</button>           
+            <button @click="afficherPremium" class="btn btn-danger">Prémium
+            </button>           
+            <ul v-if="detailsVisibles" class="list-group">
+                <li class="list-group-item">{{lePhone}}</li>
+                <li class="list-group-item">{{leMail}}</li>
+            </ul>
+        </ul>
+    </div> 
+</template>
+
+<script>
+export default {
+    props:{
+        leNom:{
+            type:String,
+            required:true
+            // required:false,
+            // default:'NomParDefaut'
+        },
+        lePhone:{
+            type:String,
+            required:true
+        },
+        leMail:{
+            type:String,
+            required:true
+        },
+        premium:{
+            type:Boolean,
+            required:false,
+            default:false,
+            //validator: function(value){return value==='1' || value==='0'}
+        }
+    },
+    data(){
+        return{
+            detailsVisibles:false,
+            unPote:{
+                id:'jojo',
+                name:"jojoLeBarjo",
+                phone:'123 12346 24',
+                email:'jojo@barjo.com',
+                
+            },
+            premiumData: this.premium
+        }
+    },
+    methods:{
+        afficherDetails(){
+            this.detailsVisibles = !this.detailsVisibles;
+        },
+        afficherPremium(){
+            this.premiumData = !this.premiumData;
+        }
+    },
+}
+</script>
