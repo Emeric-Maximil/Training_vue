ea9b86353e726d8a5cd953699f7e605105271b21
Emeric-Maximil <maximil.emeric@gmail.com>
2024-01-03 09:22:52 +0100

    Commit de tous les exos manquants pendant les vacances

---
+++ Vue_sans_globals/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <le-header></le-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+<style scoped>
+h1,h2{
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+</style>
+<script>
+import LeHeader from './components/LeHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  
+  data() {
+    return {
+      activeUser: {
+        name: 'LOL',
+        description: 'admin du site',
+        role: 'premium',
+      },
+    };
+  },
+  components:{
+    'le-header': LeHeader,
+    'badge-list': BadgeList,
+    'user-info': UserInfo,
+  }
+};
+</script>
